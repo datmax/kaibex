84d158fba4c7da6b61aba0e3fd3678bdb2518141
Max Datsun <max.datsun@student.univaq.it>
2022-02-12 04:31:57 +0100

    ada

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/',
+        permanent: true,
+      },
+    ]
+  },
+  env: {
+    NEXT_PUBLIC_APP_ID: process.env.NEXT_PUBLIC_APP_ID,
+    NEXT_PUBLIC_SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL,
+  },
 }
